e8a1544488ebb061c7c0a4a06dda62d7a5b63bda
upura <upura0@gmail.com>
2021-07-09 03:50:08 +0900

    External data

---
+++ external/processor_book.py
@@ -0,0 +1,40 @@
+import pandas as pd
+from tqdm import tqdm
+
+
+filenames = [
+    'data/cbtest_CN_test_2500ex.txt',
+    'data/cbtest_P_valid_2000ex.txt',
+    'data/cbtest_CN_train.txt',
+    'data/cbtest_V_test_2500ex.txt',
+    'data/cbtest_CN_valid_2000ex.txt',
+    'data/cbtest_V_train.txt',
+    'data/cbtest_NE_test_2500ex.txt',
+    'data/cbtest_V_valid_2000ex.txt',
+    'data/cbtest_NE_train.txt',
+    'data/cbt_test.txt',
+    'data/cbtest_NE_valid_2000ex.txt',
+    'data/cbt_train.txt',
+    'data/cbtest_P_test_2500ex.txt',
+    'data/cbt_valid.txt',
+    'data/cbtest_P_train.txt'
+]
+texts = []
+for filename in filenames:
+    with open(filename, mode='rt', encoding='utf-8') as f:
+        read_data = f.readlines()
+    body = []
+    read_data.append('\n')
+    for line in tqdm(read_data):
+        if line == '\n':
+            texts.append(''.join(body)[:-2])
+            body = []
+        else:
+            body.append(' '.join(line.split()[1:]))
+
+print(len(texts))
+df = pd.DataFrame({
+    'excerpt': texts
+})
+df.to_csv('the_childrens_book_test.csv', index=False)
+df.head()
---
+++ external/processor_dump.py
@@ -0,0 +1,28 @@
+import pandas as pd
+from tqdm import tqdm
+
+
+with open('corpus.txt', mode='rt', encoding='utf-8') as f:
+    read_data = f.readlines()
+
+titles = []
+texts = []
+is_title = True
+body = []
+read_data.append('\n')
+for line in tqdm(read_data):
+    if line == '\n':
+        is_title = True
+        texts.append(''.join(body)[:-2])
+        body = []
+    elif is_title:
+        titles.append(line.replace('\n', ''))
+        is_title = False
+    else:
+        body.append(line)
+df = pd.DataFrame({
+    'title': titles,
+    'excerpt': texts
+})
+df.to_csv('dump_of_simple_english_wiki.csv', index=False)
+df.head()
