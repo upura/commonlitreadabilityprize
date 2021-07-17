ae0a4d53ccedcbf66401dbf9b934d5b8882493e3
upura <upura0@gmail.com>
2021-07-18 00:45:12 +0900

    Merge

---
+++ submissions/ensembling.py
@@ -0,0 +1,142 @@
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import ARDRegression
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import KFold
+
+if __name__ == "__main__":
+    NUM_FOLDS = 5
+    SEED = 1000
+
+    shigeria_pred1 = np.load("shigeria_pred1.npy")
+    shigeria_pred2 = np.load("shigeria_pred2.npy")
+    shigeria_pred3 = np.load("shigeria_pred3.npy")
+    upura_pred = np.load("upura_pred.npy")
+    takuoko_exp085 = np.load("takuoko_exp085.npy")
+    takuoko_exp096 = np.load("takuoko_exp096.npy")
+    takuoko_exp105 = np.load("takuoko_exp105.npy")
+    takuoko_exp108 = np.load("takuoko_exp108.npy")
+    X_train_svd = np.load("X_train_svd.npy")
+    X_test_svd = np.load("X_test_svd.npy")
+
+    X_test = pd.DataFrame(
+        {
+            "shigeria_pred1": shigeria_pred1.reshape(-1),
+            "shigeria_pred2": shigeria_pred2.reshape(-1),
+            "shigeria_pred3": shigeria_pred3.reshape(-1),
+            "upura": upura_pred,
+            "takuoko_exp085": takuoko_exp085,
+            "takuoko_exp096": takuoko_exp096,
+            "takuoko_exp105": takuoko_exp105,
+            "takuoko_exp108": takuoko_exp108,
+        }
+    )
+    X_test = pd.concat(
+        [X_test, pd.DataFrame(X_test_svd, columns=["svd0", "svd1", "svd2"])], axis=1
+    )
+
+    # takuoko oof
+    pred_val085 = pd.read_csv("../input/commonlit-oof/pred_val085.csv")
+    pred_val096 = pd.read_csv("../input/commonlit-oof/pred_val096.csv")
+    pred_val105 = pd.read_csv("../input/commonlit-oof/pred_val105.csv")
+    pred_val108 = pd.read_csv("../input/commonlit-oof/pred_val108.csv")
+
+    pred_val105.index = pred_val105["id"]
+    pred_val105 = pred_val105.reindex(index=pred_val085["id"]).reset_index(drop=True)
+    pred_val108.index = pred_val108["id"]
+    pred_val108 = pred_val108.reindex(index=pred_val085["id"]).reset_index(drop=True)
+
+    delete_idx = pred_val085[pred_val085.id == "436ce79fe"].index
+    pred_val085 = pred_val085.drop(delete_idx).reset_index(drop=True)
+    pred_val096 = pred_val096.drop(delete_idx).reset_index(drop=True)
+    pred_val105 = pred_val105.drop(delete_idx).reset_index(drop=True)
+    pred_val108 = pred_val108.drop(delete_idx).reset_index(drop=True)
+
+    # upura oof
+    pred_val000 = pd.read_csv("../input/commonlit-oof/pred_val000.csv")
+    pred_val000.index = pred_val000["id"]
+    pred_val000 = pred_val000.reindex(index=pred_val085["id"]).reset_index(drop=True)
+
+    # shigeria oof
+    andrey_df = pd.read_csv(
+        "../input/bayesian-commonlit/np_savetxt_andrey.csv", header=None
+    ).values.ravel()
+    andrey_df2 = pd.read_csv(
+        "../input/bayesian-commonlit/np_savetxt_andrey2.csv", header=None
+    ).values.ravel()
+    andrey_df3 = pd.read_csv(
+        "../input/bayesian-commonlit/np_savetxt_andrey3.csv", header=None
+    ).values.ravel()
+
+    shigeria_val = pd.read_csv("../input/commonlit-oof/pred_val000.csv")
+    shigeria_val["pred_target"] = andrey_df
+    shigeria_val.index = shigeria_val["id"]
+    shigeria_val = shigeria_val.reindex(index=pred_val085["id"]).reset_index(drop=True)
+
+    shigeria_val2 = pd.read_csv("../input/commonlit-oof/pred_val000.csv")
+    shigeria_val2["pred_target"] = andrey_df2
+    shigeria_val2.index = shigeria_val2["id"]
+    shigeria_val2 = shigeria_val2.reindex(index=pred_val085["id"]).reset_index(
+        drop=True
+    )
+
+    shigeria_val3 = pd.read_csv("../input/commonlit-oof/pred_val000.csv")
+    shigeria_val3["pred_target"] = andrey_df3
+    shigeria_val3.index = shigeria_val3["id"]
+    shigeria_val3 = shigeria_val3.reindex(index=pred_val085["id"]).reset_index(
+        drop=True
+    )
+
+    y_train = pred_val085.true_target
+    X_train = pd.DataFrame(
+        {
+            "shigeria_pred1": shigeria_val.pred_target.values,
+            "shigeria_pred2": shigeria_val2.pred_target.values,
+            "shigeria_pred3": shigeria_val3.pred_target.values,
+            "upura": pred_val000.pred_target.values,
+            "takuoko_exp085": pred_val085.pred_target.values,
+            "takuoko_exp096": pred_val096.pred_target.values,
+            "takuoko_exp105": pred_val105.pred_target.values,
+            "takuoko_exp108": pred_val108.pred_target.values,
+        }
+    )
+    X_train = pd.concat(
+        [X_train, pd.DataFrame(X_train_svd, columns=["svd0", "svd1", "svd2"])], axis=1
+    )
+
+    y_preds = []
+    models = []
+    oof_train = np.zeros((len(X_train)))
+    cv = KFold(n_splits=NUM_FOLDS, random_state=SEED, shuffle=True)
+
+    params = {"alpha": 10, "random_state": 0}
+
+    for fold_id, (train_index, valid_index) in enumerate(cv.split(X_train)):
+        X_tr = X_train.loc[train_index, :]
+        X_val = X_train.loc[valid_index, :]
+        y_tr = y_train[train_index]
+        y_val = y_train[valid_index]
+        model = ARDRegression(
+            n_iter=300,
+            alpha_1=4.419269430437814e-05,
+            alpha_2=7.067678595336767e-08,
+            lambda_1=9886.63123193296,
+            lambda_2=71.70204561520023,
+            verbose=True,
+        )
+        model.fit(X_tr, y_tr)
+        oof_train[valid_index] = model.predict(X_val)
+        y_pred = model.predict(X_test)
+
+        y_preds.append(y_pred)
+        models.append(model)
+
+    print(mean_squared_error(oof_train, y_train, squared=False))
+    y_sub = sum(y_preds) / len(y_preds)
+
+    submission_df = pd.read_csv(
+        "../input/commonlitreadabilityprize/sample_submission.csv"
+    )
+    submission_df["target"] = y_sub
+    submission_df.to_csv("submission.csv", index=False)
+    print(submission_df.head())
---
+++ submissions/takuoko_inference.py
@@ -0,0 +1,230 @@
+import sys
+
+sys.path.append("../input/aipipeline/AIPipeline/")
+sys.path.append(
+    "../input/packages/pytorch-image-models-master/pytorch-image-models-master/"
+)
+sys.path.append(
+    "../input/packages/pretrained-models.pytorch-master/pretrained-models.pytorch-master/"
+)
+sys.path.append("../input/packages/easydict-master/easydict-master/")
+sys.path.append("../input/packages/omegaconf-master/omegaconf-master/")
+sys.path.append(
+    "../input/packages/segmentation_models.pytorch-master/segmentation_models.pytorch-master/"
+)
+sys.path.append(
+    "../input/packages/EfficientNet-PyTorch-master/EfficientNet-PyTorch-master/"
+)
+sys.path.append(
+    "../input/packages/pytorch-metric-learning-master/pytorch-metric-learning-master/src/"
+)
+sys.path.append(
+    "../input/packages/japanize-matplotlib-master/japanize-matplotlib-master/"
+)
+import gc
+import os
+import warnings
+from pathlib import Path
+
+warnings.filterwarnings("ignore")
+
+import numpy as np
+import torch
+import yaml
+from easydict import EasyDict as edict
+from hf import api, factory
+from hf.preprocesses import get_target_names, get_target_tasks, preprocess_config
+from hf.utils import get_models, load_df, setup_logger, timer
+from omegaconf import OmegaConf
+from torch.utils.data import DataLoader
+
+
+def load_config_with_omega(path, use_edict: bool = True, is_recursion: bool = False):
+    assert ".yml" in str(path) or ".json" in str(
+        path
+    ), '"path" should be ".yml" or ".json"'
+    config = OmegaConf.load(path)
+    if "_base_" in config:
+        config._base_ = [
+            b.replace(
+                "/home/user/Desktop/RistAIPipeline", "../input/aipipeline/AIPipeline"
+            )
+            .replace("config/", "../input/commonlit-config/")
+            .replace(
+                "/home/ubuntu/Desktop/RistAIPipeline", "../input/aipipeline/AIPipeline"
+            )
+            for b in config._base_
+        ]
+        for i, p in enumerate(config._base_):
+            if i == 0:
+                config_ = load_config_with_omega(p, is_recursion=True)
+            else:
+                config_ = OmegaConf.merge(
+                    config_, load_config_with_omega(p, is_recursion=True)
+                )
+        config = OmegaConf.merge(config_, config)
+
+    if not is_recursion:
+        config = yaml.load(OmegaConf.to_yaml(config, resolve=True))  # pythonのdict形式に直す
+        if use_edict:
+            config = edict(config)
+
+    return config
+
+
+def get_config(path):
+    config = load_config_with_omega(path)
+    config.data.dir = "../input/commonlitreadabilityprize/"
+    config.data.out_dir = "./"
+    config.model.params.pretrained = None
+    # config.dataloader.test.batch_size = 4
+    config.dataloader.test.num_workers = os.cpu_count()
+    config.model.device = "cuda"
+
+    return config
+
+
+# ===============
+# Functions
+# ===============
+def main(config, models):
+    with timer("split data"):
+        target_names = get_target_names(config.target)
+        target_tasks = get_target_tasks(config.target)
+        test_datasets = factory.get_test_datasets(
+            df=test,
+            ids=ids,
+            target_names=target_names,
+            target_tasks=target_tasks,
+            name=config.datasets.name,
+            params=config.datasets.test_params,
+        )
+        test_loader = DataLoader(test_datasets, **config.dataloader.test)
+
+    with timer("train"):
+        runner = api.TextPredictor(
+            target_infos=config.target, target_names=target_names
+        )
+        pred = runner.predict(test_loader, models, post_process=False)
+        del test_loader, runner
+        gc.collect()
+
+    return pred
+
+
+if __name__ == "__main__":
+    # ===============
+    # Constants
+    # ===============
+    config_path = "../input/commonlit-config/exp105_roberta_large_itpt.yml"
+    config = get_config(config_path)
+    config = preprocess_config(config)
+
+    DATA_DIR = Path(config.data.dir)
+    TEST_PATH = DATA_DIR / config.data.test_file
+    # ===============
+    # Settings
+    # ===============
+    LOGGER_PATH = "log.txt"
+    setup_logger(out_file=LOGGER_PATH)
+
+    test = load_df(TEST_PATH, dtype={config.cols.id_col: str})
+    test["text"] = test["excerpt"]
+    ids = test[config.cols.id_col].values
+
+    model_files = [
+        "../input/commonrtx2/exp105_roberta_large_itpt_fold0.pth",
+        "../input/commonrtx2/exp105_roberta_large_itpt_fold1.pth",
+        "../input/commonrtx2/exp105_roberta_large_itpt_fold2.pth",
+        "../input/commonrtx2/exp105_roberta_large_itpt_fold3.pth",
+        "../input/commonrtx2/exp105_roberta_large_itpt_fold4.pth",
+    ]
+    config_path = "../input/commonlit-config/exp105_roberta_large_itpt.yml"
+    config = get_config(config_path)
+    config = preprocess_config(config)
+    config.model.params.num_classes = [c.num_classes for c in config.target]
+    config.model.params.backbone = "../input/robertalarge/"
+    config.datasets.test_params.transform = [
+        {"name": "CleanText"},
+        {"name": "Tokenize", "params": {"model_name": config.model.params.backbone}},
+    ]
+
+    models = get_models(model_files, config)
+    pred = main(config, models)
+    del models
+    gc.collect()
+    torch.cuda.empty_cache()
+
+    model_files = [
+        "../input/commonlitrtx/exp108_roberta_large_ep10_fold0.pth",
+        "../input/commonlitrtx/exp108_roberta_large_ep10_fold1.pth",
+        "../input/commonlitrtx/exp108_roberta_large_ep10_fold2.pth",
+        "../input/commonlitrtx/exp108_roberta_large_ep10_fold3.pth",
+        "../input/commonlitrtx/exp108_roberta_large_ep10_fold4.pth",
+    ]
+    config_path = "../input/commonlit-config/exp108_roberta_large_ep10.yml"
+    config = get_config(config_path)
+    config = preprocess_config(config)
+    config.model.params.num_classes = [c.num_classes for c in config.target]
+    config.model.params.backbone = "../input/robertalarge/"
+    config.datasets.test_params.transform = [
+        {"name": "CleanText"},
+        {"name": "Tokenize", "params": {"model_name": config.model.params.backbone}},
+    ]
+
+    models = get_models(model_files, config)
+    pred2 = main(config, models)
+    del models
+    gc.collect()
+    torch.cuda.empty_cache()
+
+    model_files = [
+        "../input/commonrtx2/exp096_longformer_large_fold0.pth",
+        "../input/commonrtx2/exp096_longformer_large_fold1.pth",
+        "../input/commonrtx2/exp096_longformer_large_fold2.pth",
+        "../input/commonrtx2/exp096_longformer_large_fold3.pth",
+        "../input/commonrtx2/exp096_longformer_large_fold4.pth",
+    ]
+    config_path = "../input/commonlit-config/exp096_longformer_large.yml"
+    config = get_config(config_path)
+    config = preprocess_config(config)
+    config.model.params.num_classes = [c.num_classes for c in config.target]
+    config.model.params.backbone = "../input/longformerlarge/"
+    config.datasets.test_params.transform = [
+        {"name": "CleanText"},
+        {"name": "Tokenize", "params": {"model_name": config.model.params.backbone}},
+    ]
+
+    models = get_models(model_files, config)
+    pred3 = main(config, models)
+    del models
+    gc.collect()
+    torch.cuda.empty_cache()
+
+    model_files = [
+        "../input/commonlitrtx/exp085_electra_large_discriminator_fold0.pth",
+        "../input/commonlitrtx/exp085_electra_large_discriminator_fold1.pth",
+        "../input/commonlitrtx/exp085_electra_large_discriminator_fold2.pth",
+        "../input/commonlitrtx/exp085_electra_large_discriminator_fold3.pth",
+        "../input/commonlitrtx/exp085_electra_large_discriminator_fold4.pth",
+    ]
+    config_path = "../input/commonlit-config/exp085_electra_large_discriminator.yml"
+    config = get_config(config_path)
+    config = preprocess_config(config)
+    config.model.params.num_classes = [c.num_classes for c in config.target]
+    config.model.params.backbone = "../input/electra/large-discriminator/"
+    config.datasets.test_params.transform = [
+        {"name": "CleanText"},
+        {"name": "Tokenize", "params": {"model_name": config.model.params.backbone}},
+    ]
+
+    models = get_models(model_files, config)
+    pred4 = main(config, models)
+    del models
+    gc.collect()
+    torch.cuda.empty_cache()
+
+    np.save("takuoko_exp085", pred4["target"].reshape(-1))
+    np.save("takuoko_exp096", pred3["target"].reshape(-1))
+    np.save("takuoko_exp105", pred["target"].reshape(-1))
+    np.save("takuoko_exp108", pred2["target"].reshape(-1))
